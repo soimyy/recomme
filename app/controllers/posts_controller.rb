32a28d86f46e0f2b6ee2cd8c6b85e24ad26ab76e
soimmy <dayanbiefu7@gmail.com>
2020-02-04 22:03:53 +0900

    add destroy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,15 @@
 class PostsController < ApplicationController
 
+  #####################
   # 投稿一覧
+  #####################
   def index
     @posts = Post.all.order(created_at: :desc)
   end
 
+  #####################
   # 投稿詳細
+  #####################
   def show
 
     # URLより、投稿のidを取得する
@@ -15,11 +19,15 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: id)
   end
 
+  #####################
   # 新規作成
+  #####################
   def new
   end
 
+  #####################
   # 新規投稿
+  #####################
   def create
 
     # タイトルを取得する
@@ -35,6 +43,9 @@ class PostsController < ApplicationController
     redirect_to("/posts/index")
   end
 
+  #####################
+  # 投稿編集
+  #####################
   def edit
 
     # URLより、投稿のidを取得する
@@ -44,7 +55,9 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: id)
   end
 
+  #####################
   # 投稿更新
+  #####################
   def update
 
     # URLより、投稿のidを取得する
@@ -58,6 +71,24 @@ class PostsController < ApplicationController
 
     # 投稿を保存する
     post.save
+
+    # 投稿一覧にリダイレクトする
+    redirect_to("/posts/index")
+  end
+
+  #####################
+  # 投稿削除
+  #####################
+  def destroy
+
+    # URLより、投稿のidを取得する
+    id = params[:id]
+
+    # idに一致する投稿を探す
+    post = Post.find_by(id: id)
+    
+    # 投稿を削除する
+    post.destroy
 
     # 投稿一覧にリダイレクトする
     redirect_to("/posts/index")
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get 'posts/:id' => 'posts#show'       # 投稿詳細
   get "posts/:id/edit" => "posts#edit"  # 投稿編集
 
-  post 'posts/:id/update' => 'posts#update' # 投稿更新
-  post 'posts/create' => 'posts#create'     # 投稿新規作成
+  post 'posts/:id/destroy' = > 'posts#destroy'  # 投稿削除
+  post 'posts/:id/update' => 'posts#update'     # 投稿更新
+  post 'posts/create' => 'posts#create'         # 投稿新規作成
 
   get '/' => 'home#top'       # トップページ
   get 'about' => 'home#about' # サービス紹介ページ
