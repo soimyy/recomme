ef2ab56497e9755c44dedc85d54a821d32def888
soimmy <dayanbiefu7@gmail.com>
2020-02-07 01:40:13 +0900

    add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+
+    # バリデーション
+    validates :title, {presence: true}              # タイトル空
+    validates :title, {length: {maximum: 140}}      # タイトル文字数最大140文字
 end
